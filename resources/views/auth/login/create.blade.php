c48ecac004d704089c3982d16ecc70172c4cd506
TiborBesze <beszetibor86@googlemail.com>
2017-11-14 09:55:11 +0000

    Styled the login page

--- resources/views/auth/login/create.blade.php
+++ resources/views/auth/login/create.blade.php
@@ -3,26 +3,43 @@
 @section('description', 'Log in with your details and start sharing your favourite talks!')
 
 @section('content')
-<h1>Log into an existing account</h1>
-<form action="{{ route('auth.login.store') }}" method="POST">
-    {{ csrf_field() }}
+<div class="container">
+    <div class="col-sm-8 col-sm-offset-2">
+        <h1>Log into an existing account</h1>
 
-    <div>
-        <label for="email">E-Mail Address</label>
-        <div>
-            <input type="email" id="email" name="email" placeholder="E-Mail Address" autocomplete="email">
-        </div>
-    </div>
+        <form action="{{ route('auth.login.store') }}" method="POST">
+            {{ csrf_field() }}
 
-    <div>
-        <label for="password">Password</label>
-        <div>
-            <input type="password" id="password" name="password" placeholder="Password">
-        </div>
-    </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="control-label">E-Mail Address</label>
+                <div>
+                    <input type="email" id="email" class="form-control" name="email" placeholder="E-Mail Address" autocomplete="email">
+                </div>
 
-    <div>
-        <button type="submit">Login</button>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        {{ $errors->first('email') }}
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="control-label">Password</label>
+                <div>
+                    <input type="password" id="password" class="form-control" name="password" placeholder="Password">
+                </div>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        {{ $errors->first('password') }}
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
     </div>
-</form>
+</div>
 @endsection
