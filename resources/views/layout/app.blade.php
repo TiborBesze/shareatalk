6f2152b3e6706ce25a688658d0dc3115a68f34ea
TiborBesze <beszetibor86@googlemail.com>
2017-11-14 09:17:41 +0000

    Added frontend assets

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,9 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', env('APP_NAME')) | {{ env('APP_NAME') }}</title>
         <meta name="description" content="@yield('description', env('APP_DESCRIPTION'))">
+        <link rel="stylesheet" href="{{ mix('css/shareatalk.css') }}">
     </head>
     <body>
         @include('layout.partials._nav')
         @yield('content')
+        <script src="{{ mix('js/shareatalk.js') }}"></script>
     </body>
 </html>
