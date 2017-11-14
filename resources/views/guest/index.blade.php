96f67ac4bbfbe09d1da4f558ec082c4451032bbf
TiborBesze <beszetibor86@googlemail.com>
2017-11-14 10:34:43 +0000

    Styled the guest home page

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -3,10 +3,25 @@
 @section('description', env('APP_DESCRIPTION'))
 
 @section('content')
-    <div>
-        <a href="{{ route('auth.register.create') }}">Register an account</a>
+<div class="container">
+    <div class="col-sm-8 col-sm-offset-2">
+        <h1 class="text-center">
+            Welcome to Share-A-Talk!
+        </h1>
+
+        <h3 class="text-center">
+            <i>A social network for sharing your favourite talks with the world</i>
+        </h3>
+
+        <hr>
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="{{ route('auth.register.create') }}" class="btn btn-primary btn-block col-sm-6">Register an account</a>
+            </div>
+            <div class="col-sm-6">
+                <a href="{{ route('auth.login.create') }}" class="btn btn-info btn-block col-sm-6">Log into an existing account</a>
+            </div>
+        </div>
     </div>
-    <div>
-        <a href="{{ route('auth.login.create') }}">Log into an existing account</a>
-    </div>
+</div>
 @endsection
