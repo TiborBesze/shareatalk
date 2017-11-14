6ff175a7387e952e8760dc90226a20b55bfcea4d
TiborBesze <beszetibor86@googlemail.com>
2017-11-14 10:37:19 +0000

    Changed alert style

--- resources/views/layout/partials/_alert.blade.php
+++ resources/views/layout/partials/_alert.blade.php
@@ -1,8 +1,10 @@
 <div class="container">
-    <div class="alert alert-{{ session()->get('flash_message_type') ?? 'success' }} alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        {{ session()->get('flash_message') }}
+    <div class="col-sm-8 col-sm-offset-2">
+        <div class="alert alert-{{ session()->get('flash_message_type') ?? 'success' }} alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            {{ session()->get('flash_message') }}
+        </div>
     </div>
 </div>
